1ac6006f8229a615cf5fcdd8dac468ccda3456e5
LuisHMaltez <00012321@uca.edu.sv>
2025-06-13 07:39:17 -0600

    Projecto base

---
+++ videojuegos/src/test/java/org/ncapas/videojuegos/VideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ncapas.videojuegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideojuegosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
