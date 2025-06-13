b46cd307d199d9057814aad7689e95e4064c9647
LuisHMaltez <00012321@uca.edu.sv>
2025-06-13 08:37:16 -0600

    Add Repositoy

---
+++ videojuegos/src/main/java/org/ncapas/videojuegos/repository/iVideojuegoRepository.java
@@ -0,0 +1,8 @@
+package org.ncapas.videojuegos.repository;
+
+import org.ncapas.videojuegos.entity.Videojuego;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface iVideojuegoRepository extends JpaRepository<Videojuego, Long>{
+
+}
